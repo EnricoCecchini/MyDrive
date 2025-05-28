814489717aac6b55cf2da4fd185e7953d541721f
EnricoCecchini <enrico.cecchini243@gmail.com>
2025-05-27 22:04:38 -0600

    Added Login and Register to Router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Login from './routes/auth/Login'
 import Landing from './routes/Landing'
+import Register from './routes/auth/Register'
 
 function App() {
 
     return (
         <Router>
             <Routes>
-                <Route path='/login' element={<Login />} />
                 <Route path='/' element={<Landing />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
             </Routes>
         </Router>
     )
