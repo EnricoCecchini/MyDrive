17b6e0cbcdd287d5723ce4a6857491ac4162f5ae
EnricoCecchini <enrico.cecchini243@gmail.com>
2025-05-28 23:38:47 -0600

    Search bar in navbar

--- client/src/components/inputs/SearchInput.tsx
+++ client/src/components/inputs/SearchInput.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 function SearchInput() {
-  return (
-    <div className='flex flex-row flex-nowrap bg-indigo-300 p-2 rounded-2xl gap-x-2'>
-        <input className='h-full w-full focus:outline-none' type="text" name="search" placeholder='Search'/>
-        <SearchOutlinedIcon sx={{fontSize: "2rem"}}/>
-    </div>
-  )
+    const [searchText, setSearchText] = useState<string>("")
+
+    const handleUpdateSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault()
+        setSearchText(e.target.value)
+    }
+
+    const handleSearch = () => {
+        console.log(searchText)
+    }
+
+    return (
+        <div className='flex flex-row flex-nowrap bg-indigo-300 p-2 rounded-2xl gap-x-2'>
+                <input className='h-full w-full focus:outline-none' type="text" name="search" placeholder='Search' onChange={handleUpdateSearch} />
+                <span onClick={handleSearch}><SearchOutlinedIcon sx={{fontSize: "2rem"}} /></span>
+        </div>
+    )
 }
 
 export default SearchInput
