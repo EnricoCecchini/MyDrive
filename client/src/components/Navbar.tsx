8547b9ea5acd556b00cec0173e28886b379f0d73
EnricoCecchini <enrico.cecchini243@gmail.com>
2025-05-28 23:03:33 -0600

    Begin navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Navbar() {
+    const [isExpanded, setIsExpanded] = useState<boolean>(false)
+
     const options = [
         {name: "Home", url: "/"},
         {name: "Recent Files", url: "/recent"},
@@ -9,18 +11,22 @@ function Navbar() {
 
     return (
         <>
-            <div className='flex flex-row w-full justify-between p-4'>
-                <div className='flex items-start'>
+            <div className='flex flex-row w-full justify-between items-center p-2 bg-indigo-400 text-white font-bold text-2xl'>
+                <div className='flex items-start gap-x-4'>
+                    <span className='text-4xl italic px-4'>MyDrive</span>
                     {options.map((item) => {
                         return (
-                            <div key={item.name} className='px-2'>
-                                <span>{item.name}</span>
-                            </div>
+                            <span
+                                key={item.name}
+                                className='h-full p-2 rounded-lg hover:bg-indigo-800'
+                            >
+                                <a href={item.url}>{item.name}</a>
+                            </span>
                         )
                     })}
                 </div>
                 <div className='flex items-end'>
-                    <span>Profile</span>
+                    <span className='h-full p-2 rounded-lg hover:bg-indigo-800'>Profile</span>
                 </div>
             </div>
         </>
